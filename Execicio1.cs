b2de304208226b0d6b2088904aca45daaad9835c
joaopsilva59-ops <joao.p.silva59@edu.senai.br>
2026-02-19 11:32:17 -0300

    Entregando Ex1, 2 e 3

---
+++ Execicio1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// -- Arquivo Exercicio1.cs
+public static class Exercicio1
+{
+    public static void Executar()
+    {
+		    Console.WriteLine();
+		    
+        Console.WriteLine("Hello World");
+        Console.WriteLine();
+    }
+}
---
+++ Exercicio2.cs
@@ -0,0 +1,12 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+    //private static string nome = "João Pedro";
+    
+    public static void Executar()
+    {
+        string nome = "João Pedro";
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
